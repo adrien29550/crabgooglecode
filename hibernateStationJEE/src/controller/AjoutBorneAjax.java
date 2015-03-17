29cc2d27cd93d16722e456b1d8f60785695bda1c
truillet-a <truillet-a@acr-M4610G-315.tec.saint-louis.local>
2015-03-17 14:53:11 +0100

    !!!

--- hibernateStationJEE/src/controller/AjoutBorneAjax.java
+++ hibernateStationJEE/src/controller/AjoutBorneAjax.java
@@ -27,7 +27,7 @@ public class AjoutBorneAjax extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
-//Nuggets and M&M's
+//Nuggets and
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
